49739e5633206f07e7ff948e19adccd6ace1cb0e
moenage <moenage7@yahoo.com>
2024-01-13 16:09:21 -0800

    Added enemy spawner

--- Scenes/Enemies/enemy_1.gd
+++ Scenes/Enemies/enemy_1.gd
@@ -6,7 +6,7 @@ var speed: float = 200.0
 var target: CharacterBody2D
 
 func _ready():
-	target = get_parent().get_node("Player")
+	target = get_parent().get_parent().get_node("Player")
 
 func _physics_process(_delta):
 	velocity = position.direction_to(target.position) * speed
--- Scenes/Levels/level.gd
+++ Scenes/Levels/level.gd
@@ -1,10 +1,28 @@
 extends Node2D
 
+@export var minxCoord: int = -640
+@export var minyCoord: int = -135
+@export var maxxCoord: int = 2563
+@export var maxyCoord: int = 1400
+
 var laserScene: PackedScene = preload("res://Scenes/Projectiles/laser.tscn")
 
+var enemyScene: PackedScene = preload("res://Scenes/Enemies/enemy_1.tscn")
+
+#Spawn Bullet from player
 func _on_player_shot_laser(pos, laserDir):
 	var laser = laserScene.instantiate() as Area2D
 	laser.position = pos
 	laser.rotation_degrees = rad_to_deg(laserDir.angle()) + 90
 	laser.direction = laserDir
 	$Projectiles.add_child(laser)
+
+
+#Spawn enemies within the arena borders
+func _on_spawn_timer_timeout():
+	var enemy = enemyScene.instantiate() as CharacterBody2D
+	enemy.position = Vector2(randf_range(minxCoord,maxxCoord), 
+	randf_range(minyCoord,maxyCoord))
+	$Enemies.add_child(enemy)
+	$SpawnTimer.wait_time -= 0.1
+
